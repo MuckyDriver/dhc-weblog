536d07524ce7ed53c8665ed7bce16264848f4d5c
DHCGamer1055 <71794495+MuckyDriver@users.noreply.github.com>
2025-03-30 18:20:23 +0100

    dhcweblog release 2.0.7
    Small content update where I updated my skills and knowledge section.

--- scripts/js/disclaimer.jquery.js
+++ scripts/js/disclaimer.jquery.js
@@ -1,6 +1,6 @@
 $(function() {
     const disclaimer = $(document.createElement("div"))
-    const ver = "2.0.6";
+    const ver = "2.0.7";
     const msg2 = "Website main search feature will not work!";
 
     disclaimer.addClass("disclaimer")
